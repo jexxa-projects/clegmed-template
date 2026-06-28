4579dba7f4b674fddc8946e3e0f51f6523e4b781
repplix <116941865+repplix@users.noreply.github.com>
2026-06-28 16:08:01 +0200

    chore(build) cleanup includes

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -1,22 +1,13 @@
-
-#include <semaphore>
-
-#include "clegmed/core/CLegMed.hpp"
-#include "clegmed/core/flowgraph/FlowGraph.hpp"
 #include "clegmed/plugins/shortcuts.hpp"
-#include "clegmed/plugins/generic/GenericConsumer.hpp"
-#include "clegmed/plugins/generic/GenericProcessor.hpp"
-#include "clegmed/plugins/generic/GenericProducer.hpp"
-
 
 int main() {
     using namespace clegmed::shortcuts;
 
     auto flowgraph = FlowGraph{}
-    .every(2s)
-    .from(emit("Hello World"))
-    .then(passThrough())
-    .consumeWith(logInfo());
+        .every(2s)
+        .from(emit("Hello World"))
+        .then(passThrough())
+        .consumeWith(logInfo());
 
     auto clegmed = CLegMed(std::move(flowgraph));
     clegmed.run();
